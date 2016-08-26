7ac3ced1e30a18ff849b851016c974fdafdafa40
d8ahazard <bklomsten@264BELL.COM>
2016-08-26 12:49:52 -0500

    Test again

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-
+# Add a comment line to see if changes work...
 # We're relocating the build project to a subdirectory, then using symlinks
 # to expose the subdirectories where they used to be. If the manifest hasn't
 # been updated, we need to include all the subdirectories.
